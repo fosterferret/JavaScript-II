3079945a6d4efc6cbfcc276d06aed12dca425ec9
Francis Bulus <notchera@gmail.com>
2019-08-20 15:24:47 +0100

    solved problem 1 under challenge 5 in array-methods

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -93,6 +93,11 @@ console.log(ticketPriceTotal);
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+let largeDonors = [];
+largeDonors = runners.filter(runner => {
+  return runner.donation > 200;
+});
+console.log(largeDonors);
 
 // Problem 2
 
