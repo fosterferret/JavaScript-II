cad1b96bf4a96226638b6fb9ce01146d986ac596
Francis Bulus <notchera@gmail.com>
2019-08-20 13:20:25 +0100

    updated last solution in callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -65,9 +65,9 @@ function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
   if (list.includes(item)) {
-    return cb(true);
+    cb(true);
   } else {
-    return cb (false);
+    cb (false);
   }
 }
 
