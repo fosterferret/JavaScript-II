4c56061d99a92ecced590f72afc144b3bfc31ec7
Francis Bulus <notchera@gmail.com>
2019-08-20 12:32:25 +0100

    solved challenge 1 in closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -4,6 +4,19 @@
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
 
+let somethingSayer = (something) => {
+  let theSomething = something;
+
+  let saySomething = () => {
+    console.log(`${theSomething}!`)
+  }
+
+  return saySomething; 
+}
+
+let lambda = somethingSayer('Lambda')
+lambda();
+
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
